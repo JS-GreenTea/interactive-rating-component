43ef69dfcf84f85fed04db6a0c5b8526834a8b71
2yunseong <dbsdltjd3701@naver.com>
2022-09-17 22:23:40 +0900

    feat: onSubmit 구현

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function onClick(e) {
   rating = e.target.value;
   console.log(rating);
 }
+
+function onSubmit() {
+  if (rating === -1) {
+    alert("Please select number");
+    return;
+  }
+  ratingComponent.style.display = "none";
+  thankYouComponent.style.display = "block";
+  const ratingResult = document.querySelector("#rating-result");
+  ratingResult.innerText = `You selected out ${rating} of 5`;
+}
