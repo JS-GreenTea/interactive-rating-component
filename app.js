1103d1fcf4b0f3b303c8b6ae56574c5c26364977
KINHYEONJI <hyeonji0718@gmail.com>
2022-09-17 19:58:20 +0900

    feat(app) : 제출 버튼 클릭 함수 구현
    - 아무것도 선택 된 것이 없을때 알림창 출력
    - 바르게 제출 했을 때 "감사합니다"창 출력

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 let numDiv = document.getElementsByClassName("num-btn");
+let submitBtn = document.getElementsByClassName("submit-btn")[0];
+let container = document.getElementsByClassName("container")[0];
+let afterContainer = document.getElementsByClassName("after-container")[0];
+let selectedInfo = document.getElementsByClassName("selected-info")[0];
 let selectNum;
+
 for (let i = 0; i < numDiv.length; i++) {
   numDiv[i].addEventListener("click", (e) => clickNumBtn(e));
-  console.log(numDiv[i]);
 }
 
 function clickNumBtn(event) {
   removeClickd();
   event.target.classList.add("clicked");
   selectNum = event.target.innerText;
-  console.log(selectNum);
 }
 
 function removeClickd() {
   for (let i = 0; i < numDiv.length; i++) {
     numDiv[i].classList.remove("clicked");
   }
+}
+
+submitBtn.addEventListener("click", clickSubmitBtn);
+
+function clickSubmitBtn() {
+  if (selectNum === undefined) {
+    alert("Please, click button");
+  } else {
+    clearContainer();
+    showThxView();
+  }
+}
+
+function clearContainer() {
+  container.style.display = "none";
+}
+
+function showThxView() {
+  afterContainer.style.display = "";
+  selectedInfo.innerText = `You selected ${selectNum} out of 5`;
 }
