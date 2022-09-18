d1fead4bfd3438f2e87e186fd62efd69e3ef7308
2yunseong <dbsdltjd3701@naver.com>
2022-09-18 23:28:01 +0900

    feat: 레이팅 상태 핸들링 로직 작성
    Co-authored-by: MINJOO-KIM <MINJOO-KIM@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,11 +3,35 @@ const ratingComponent = document.querySelector(".rating-component");
 const thankYouComponent = document.querySelector(".thank-you-component");
 const ratingButtons = document.querySelectorAll(".rating-button");
 const submitButton = document.querySelector(".submit-button");
+const buttonStates = [
+  { id: 1, isSelected: false },
+  { id: 2, isSelected: false },
+  { id: 3, isSelected: false },
+  { id: 4, isSelected: false },
+  { id: 5, isSelected: false },
+];
 
-function onClick(e) {
+const onClick = (e) => {
   rating = e.target.value;
   console.log(rating);
-}
+  if (!findSelectButtonById(rating).isSelected) {
+    ratingButtons.forEach((ratingButton) => {
+      ratingButton.classList.remove("select");
+    });
+    buttonStates.forEach((buttonState) => {
+      buttonState.isSelected = false;
+    });
+
+    const selectedButton = document.getElementById(`button-${e.target.value}`);
+    selectedButton.classList.add("select");
+    findSelectButtonById(rating).isSelected = true;
+  }
+};
+
+const findSelectButtonById = (value) => {
+  console.log(buttonStates.filter((buttonState) => buttonState.id === +value));
+  return buttonStates.filter((buttonState) => buttonState.id === +value)[0];
+};
 
 function onSubmit() {
   if (rating === -1) {
